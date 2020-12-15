82612dcf50dfe5ac4108579a475d99105161e468
salaza <gun159@outlook.com>
2020-12-15 16:19:41 +0700

    "lab5_1.cpp completed"

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,8 +2,24 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
+    int number=1;
+    int even = 0, odd = 0;
+    while(number != 0 ){
+        cout << "Enter an integer: ";
+        cin >> number;
+        if(number > 0){
+            if(number%2 ==0 && number != 0){
+            even++;
+            }
+            else{
+            odd++;
+            }   
+        }
+         
+    }
     cout << "#Even numbers = ";
+    cout << even << endl;
     cout << "#Odd numbers = ";
+    cout << odd;
     return 0;
 }
